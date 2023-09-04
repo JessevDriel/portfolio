95c41992ed6a2bff7917f0df8960ac1d80535da1
JessevDriel <088878@glr.nl>
2023-09-04 12:03:56 +0200

    website upload
    upload progress week 1

---
+++ src/js/js.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll", function () {
+const distance = window.scrollY
+console.log(distance);
+
+
+// if(distance > 200){
+//     document.querySelector(".animation1").classList.add("slideIn1");
+// }
+
+if(distance > 700){
+    document.querySelector(".animation3").classList.add("slideIn3");
+}
+if(distance > 700){
+    document.querySelector(".animation4").classList.add("slideIn4");
+}
+if(distance > 900){
+    document.querySelector(".animation5").classList.add("slideIn5");
+}
+if(distance > 900){
+    document.querySelector(".animation6").classList.add("slideIn6");
+}
+
+
+})  
+
+function slideIn() {
+        document.querySelector(".animation2").classList.add("slideIn2");
+ 
+}
+
